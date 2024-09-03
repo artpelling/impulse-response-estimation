c8fe94dc717816c4064013d67e498c7395b0bdcf
Art Pelling <a.pelling@tu-berlin.de>
2024-09-03 15:03:34 +0200

    numerics

--- code/script.py
+++ code/script.py
@@ -1,44 +1,74 @@
 #!/usr/bin/env python3
 
-from scipy.optimize import minimize
+import numpy as np
 import scipy.linalg as spla
+import scipy.signal as ss
 import matplotlib.pyplot as plt
 import pyfar as pf
 from pathlib import Path
 
+from pymor.algorithms.to_matrix import to_matrix
+
 from solver import solve_regularized_toeplitz_LS
+from kernel import construct_toeplitz_operator
 
 
 plt.close('all')
 
-files = {
-    'basilica': 'KE_Basilica_of_Eberbach_Monastery.wav',  # T=4.6
-    'gewandhaus': 'GH_Gewandhaus.wav',  # T=1.9
-    'chamber': 'KE_Chamber_Music_Hall_of_Konzerthaus_Berlin.wav',  # T=1.2
-    'murakuni': 'MK_Murakuni-Za.wav',   # T=0.9
-}
-
-file = Path('../resources/rirs_simulated') / files['murakuni']
-ir = pf.io.read_audio(file)
-
 file = Path('/home/pelling/data/fabian/SOFA/FABIAN_HRIR_measured_HATO_0.sofa')
 ir = pf.io.read_sofa(file)[0][0,0]
 
-n = 500
-u = pf.signals.exponential_sweep_time(n-ir.n_samples+1, (10, 22000))
+n_sweep = 512
+u = pf.signals.exponential_sweep_time(n_sweep, (100, 22000))
 y = pf.dsp.convolve(u, ir)
 hexp = pf.dsp.deconvolve(y, u)
 
-ir = pf.dsp.pad_zeros(ir, n-ir.n_samples)
-hexp = pf.dsp.pad_zeros(hexp, n-hexp.n_samples)
+n_taps = 256
+K = construct_toeplitz_operator(u.time[0], y.n_samples, n_taps)
+k = to_matrix(K)
+U, sv, Vh = spla.svd(k, full_matrices=False)
+plot_style = {"lw": 1, "fillstyle": 'none', 'markersize': 5}
+fig, ax = plt.subplots(figsize=(16/3, 3), dpi=300, constrained_layout=True)
+ax.semilogy(sv, '.-', label='$\sigma_i(K)$', **plot_style)
+uTy = y.time[0]@U
+ax.semilogy(np.abs(uTy), '.-', label='$|u_i^Ty|$', **plot_style)
+ax.semilogy(np.abs(uTy)/sv, '.-', label='$|u_i^Ty|/\sigma_i$', **plot_style)
+ax.set_xlim((0, n_taps))
+plt.legend()
+plt.savefig('svs.png')
 
-options = {'disp': True, 'gtol': 1e-8}
-hopt = solve_regularized_toeplitz_LS(u, y, tol=0, method='BFGS', options=options)
 
-pf.plot.time(ir-hexp)
-pf.plot.time(ir-hopt)
-#pf.plot.time(hopt)
-#plt.xlim(np.array((-1,5))/44100)
-#plt.ylim(np.array((-0.3,0.3))*1e-5)
-#plt.plot(ir.times, np.exp(-np.linspace(0,10,n)))
-plt.savefig('plot.png')
+# ALGO
+alpha = 0.825
+frq = (100, 8000)
+beta = ss.firls(101, np.array([0, frq[0], frq[0], frq[1], frq[1], 22050]), np.array([1, 1, 0, 0, 1, 1]), fs=44100)
+beta = ss.minimum_phase(beta)
+regularization = {'lam': 0, 'alpha': alpha, 'beta': beta}
+
+# method can be either 'lstsq' or 'BFGS'
+solver_kwargs = {'init': None, 'method': 'lstsq', 'tol': 1e-12, 'options': {'disp': True, 'gtol': 1e-8}}
+
+hopt = solve_regularized_toeplitz_LS(u, y, n_taps, **solver_kwargs, **regularization)
+
+
+# PLOTTING
+dB = True
+if n_taps > hexp.n_samples:
+    ir = pf.dsp.pad_zeros(ir, hopt.n_samples-ir.n_samples)
+    hexp = pf.dsp.pad_zeros(hexp, hopt.n_samples-hexp.n_samples)
+else:
+    ir = pf.dsp.pad_zeros(ir, hexp.n_samples-ir.n_samples)
+    hopt = pf.dsp.pad_zeros(hopt, hexp.n_samples-hopt.n_samples)
+
+fig, ax = plt.subplots(figsize=(16/3, 3), dpi=300, constrained_layout=True)
+pf.plot.time((ir-hexp)/ir, dB=dB, unit='samples', label='pyfar', **plot_style)
+pf.plot.time((ir-hopt)/ir, dB=dB, unit='samples', label=solver_kwargs['method'], **plot_style)
+ax.set_xlim((0, n_taps))
+plt.legend()
+plt.savefig('errtime.png')
+
+fig, ax = plt.subplots(figsize=(16/3, 3), dpi=300, constrained_layout=True)
+pf.plot.freq(ir-hexp, dB=dB, label='pyfar', **plot_style)
+pf.plot.freq(ir-hopt, dB=dB, label=solver_kwargs['method'], **plot_style)
+plt.legend()
+plt.savefig('errfreq.png')
--- code/solver.py
+++ code/solver.py
@@ -3,65 +3,50 @@ import numpy as np
 import pyfar as pf
 import scipy.linalg as spla
 import scipy.optimize as spopt
-from pymor.operators.numpy import NumpyToeplitzOperator, NumpyMatrixOperator
+
+from pymor.algorithms.to_matrix import to_matrix
+
+from kernel import construct_filter_operator, construct_toeplitz_operator
 
 
-def solve_regularized_toeplitz_LS(u, y, **solver_kwargs):
+def solve_regularized_toeplitz_LS(u, y, n_taps, lam=1, alpha=0.9, beta=np.array([1]), init=0, method='qr', **solver_kwargs):
     assert u.sampling_rate == y.sampling_rate
     sampling_rate = u.sampling_rate
 
-    x0 = np.squeeze(pf.dsp.deconvolve(y, u).time)
-    #x0 = np.zeros_like(x0)
+    # starting solution 0 or deconv
+    x0 = np.squeeze(pf.dsp.deconvolve(y, u).time)[:n_taps]
+    if init == 0:
+        x0 = np.zeros_like(x0)
+    elif init == 'rand':
+        x0 = np.random.randn(*x0.shape)
+    n = x0.shape[0]
 
     u, y = np.squeeze(u.time), np.squeeze(y.time)
     assert u.ndim == y.ndim == 1
 
     # construct Toeplitz operator
-    c = np.zeros_like(y)
-    c[:len(u)] = u
-    r = np.zeros_like(y)
-    r[0] = c[0]
-    K = NumpyToeplitzOperator(c, r=r)
-    f = pf.dsp.filter.butterworth(pf.signals.impulse(x0.shape[0]), 2, 2000, btype='highpass').time.T
-    f = f.reshape(-1, 1, 1)
+    K = construct_toeplitz_operator(u, len(y), n_taps)
+    
+    F = lam*construct_filter_operator(n_taps, alpha, beta, delay=0)
 
-    c = np.zeros_like(f)
-    c[0] = f[0]
-    F = NumpyToeplitzOperator(c=c, r=f)  # upper triangular F
+    if method == 'lstsq':
+        K, F = to_matrix(K), to_matrix(F)
+        x = spla.lstsq(K.T@K + F, K.T@y)[0]
+    else:
+        # define cost function
+        def fun(x):
+            xpm = K.source.from_numpy(x)
+            Kx = K.apply(xpm).to_numpy().T
+            f = F.apply(xpm).to_numpy().T
+            return 0.5*(spla.norm(Kx-y)**2 + spla.norm(f)**2)
 
-    import scipy.sparse as sps
-    T60 = 0.9
-    alpha = 1e-3**(1/T60/44100)
+        # define jacobian
+        def jac(x):
+            xpm = K.source.from_numpy(x)
+            ypm = K.range.from_numpy(y)
+            J = K.apply_adjoint(K.apply(xpm)-ypm) + F.apply_adjoint(F.apply(xpm))
+            return J.to_numpy().squeeze()
 
-    alpha = 0.6
-    print(alpha)
-    n = len(y)
-    ai = alpha**(np.arange(n)+1)
-    d = (ai*(1-alpha))**-0.5
-    d[-1] = ai[-1]**-0.5
-    d *= 1
-    m = 1e6
-    d[d > m] = m
-    print(d)
-    d /= np.max(d)
-    D = NumpyMatrixOperator(sps.dia_array((d, 0), shape=(n, n)))
-
-    # define cost function
-    def fun(x):
-        xpm = K.source.from_numpy(x)
-        Kx = K.apply(xpm).to_numpy().T
-        f = D.apply(F.apply(xpm)).to_numpy().T
-        return 0.5*(spla.norm(Kx-y)**2 + f.T@f)
-
-    # define jacobian
-    def jac(x):
-        xpm = K.source.from_numpy(x)
-        ypm = K.range.from_numpy(y)
-        J = K.apply_adjoint(K.apply(xpm)-ypm) + F.apply_adjoint(D.apply_adjoint(D.apply(F.apply(xpm))))
-        return np.squeeze(J.to_numpy().T)
-
-    x0 = np.squeeze(K.source.random(1, distribution='normal').to_numpy().T)
-
-    x = spopt.minimize(fun, x0, jac=jac, **solver_kwargs)['x']
+        x = spopt.minimize(fun, x0, jac=jac, **solver_kwargs)['x']
 
     return pf.Signal(x, sampling_rate=sampling_rate)
